696304bc26e81aa77b0e8abcd244acfffdd5f474
Rhay from Workstation <javier.johnrhay@gmail.com>
2026-05-17 09:55:11 +0800

    project starter

---
+++ app/Http/Controllers/SampleController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Sample;
+
+class SampleController extends Controller
+{
+    public function index()
+    {
+        $sample = Sample::query()->first();
+        $messageFromDatabase = $sample?->message ?? 'No message found in the database.';
+        return response()->json(['message' => $messageFromDatabase]);
+    }
+}
